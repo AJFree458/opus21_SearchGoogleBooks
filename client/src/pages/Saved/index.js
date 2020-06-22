ccf64037e0b13cc0675eee98820cbb5be43d4534
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-06-22 12:40:59 -0400

    Remove a component to simplify the app

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,11 +1,17 @@
 import React from "react";
-import SavedField from "../../components/SavedField";
+import SavedCard from "../../components/SavedCard";
 
 function Saved() {
   return(
     <div className="container-xl mb-3">
-      <SavedField />
-
+      <div className="card mt-3">
+        <div className="card-header">
+          <h3>Saved Books</h3>
+        </div>
+        <div className="card-body">
+          <SavedCard />
+        </div>
+      </div>
     </div>
   )
 };
