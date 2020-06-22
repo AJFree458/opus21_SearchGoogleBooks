da5b51637939db5bfe1335933f04a54c8066acad
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-06-21 20:00:43 -0400

    Add router for the pages of the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Jumbotron from "./components/Jumbotron";
+import Saved from "./pages/Saved";
+import Search from "./pages/Search";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <NavBar />
+        <Jumbotron />
+        <Switch>
+          <Route exact path="/">
+            <Search />
+          </Route>
+          <Route exact path="/saved">
+            <Saved />
+          </Route>
+        </Switch>
       </div>
-    );
-  }
+    </Router>
+  )
 }
 
 export default App;
