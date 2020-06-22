1458007c5e8da02dac6420cfd0c8c8e5b1414330
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-06-22 12:43:15 -0400

    Remove a component to simplify the app

--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import InputCard from "../../components/SearchCard";
-import ResultsField from "../../components/ResultsField"
+import ResultsCard from "../../components/ResultsCard"
 
 function Search() {
   return(
     <div className="container-xl mb-3">
       <InputCard />
 
-      <ResultsField />
+      <div className="card mt-3">
+        <div className="card-header">
+          <h3>Results</h3>
+        </div>
+        <div className="card-body">
+          <ResultsCard />
+        </div>
+      </div>
 
     </div>
   )
